ea64db0e79eb9d54cdb70960c595346cc7e90c60
Kopcoder <jsk89@pitt.edu>
2023-11-16 15:23:22 -0500

    style tweaks

--- lib/view/character_sheet_view.dart
+++ lib/view/character_sheet_view.dart
@@ -51,7 +51,7 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
       appBar: AppBar(
         backgroundColor: Color(0xFFAD9090),
         title: Text(vm.name,
-            style: TextStyle(fontSize: 24, color: Colors.black)),
+            style: TextStyle(fontSize: 24, color: const Color(0xFF302727))),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -59,31 +59,43 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
             Row(
               children: [
                 Container(
-                  margin: EdgeInsets.all(25.0),
+                  margin: EdgeInsets.fromLTRB(25, 25, 25, 0),
                   width: 80.0,
                   height: 80.0,
-                  color: Color(0xFFAD9090),
                   alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      border: Border.all(color:const Color(0xFF302727)),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFFAD9090),
+                  ),
                   child: Text('${vm.armorClass} \n Armor Class',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center),
                 ),
                 Container(
-                  margin: EdgeInsets.all(25.0),
+                  margin: EdgeInsets.fromLTRB(25, 25, 25, 0),
                   width: 80.0,
                   height: 80.0,
-                  color: Color(0xFFAD9090),
                   alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFAD9090),
+                      border: Border.all(color:const Color(0xFF302727)),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
                   child: Text('+${vm.abilityModifiers[1]} \nInitiative',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center),
                 ),
                 Container(
-                  margin: EdgeInsets.all(25.0),
+                  margin: EdgeInsets.fromLTRB(25, 25, 25, 0),
                   width: 80.0,
                   height: 80.0,
-                  color: Color(0xFFAD9090),
                   alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFAD9090),
+                      border: Border.all(color:const Color(0xFF302727)),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
                   child: Text('${vm.speed} \nSpeed',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center),
@@ -93,21 +105,29 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
             Row(
               children: [
                 Container(
-                  margin: EdgeInsets.all(25.0),
+                  margin: EdgeInsets.fromLTRB(25, 25, 25, 0),
                   width: 80.0,
                   height: 80.0,
-                  color: Color(0xFFAD9090),
                   alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFAD9090),
+                      border: Border.all(color:const Color(0xFF302727)),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
                   child: const Text('+2 \n Proficiency',
                       style: TextStyle(fontSize: 14),
                       textAlign: TextAlign.center),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(25, 25, 0, 25),
+                  margin: EdgeInsets.fromLTRB(25, 25, 0, 0),
                   width: 80.0,
                   height: 80.0,
-                  color: Color(0xFFAD9090),
                   alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFAD9090),
+                      border: Border.all(color:const Color(0xFF302727)),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
                   child: Text('${vm.currHitPoints}/${vm.maxHitPoints} \n Hit Points',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center),
@@ -116,8 +136,8 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                   margin: EdgeInsets.all(0.0),
                   child: Column(children: <Widget>[
                     SizedBox(
-                      width: 40,
-                      height: 40,
+                      width: 30,
+                      height: 30,
                       child: FilledButton(
                         onPressed: incrementHealth,
                         style: FilledButton.styleFrom(
@@ -125,13 +145,13 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                         ),
                         child: const Text(
                           '+',
-                          style: TextStyle(fontSize: 30),
+                          style: TextStyle(fontSize: 20),
                         ),
                       ),
                     ),
                     SizedBox(
-                        width: 40,
-                        height: 40,
+                        width: 30,
+                        height: 30,
                         child: FilledButton(
                           onPressed: decrementHealth,
                           style: FilledButton.styleFrom(
@@ -139,17 +159,21 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                           ),
                           child: const Text(
                             '-',
-                            style: TextStyle(fontSize: 30),
+                            style: TextStyle(fontSize: 20),
                           ),
                         ))
                   ]),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(10, 25, 25, 25),
+                  margin: EdgeInsets.fromLTRB(20, 25, 25, 0),
                   width: 80.0,
                   height: 80.0,
-                  color: Color(0xFFAD9090),
                   alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFAD9090),
+                      border: Border.all(color:const Color(0xFF302727)),
+                      borderRadius: BorderRadius.circular(10)
+                  ),
                   child: Text('1d${vm.hitDice} \nHit Dice',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center),
@@ -176,7 +200,10 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                       margin: EdgeInsets.fromLTRB(17.0, 10.0, 10.0, 0),
                       width: 90.0,
                       height: 70.0,
-                      decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                      decoration: BoxDecoration(
+                          border: Border.all(color:const Color(0xFF302727), width: 3),
+                          borderRadius: BorderRadius.circular(10)
+                      ),
                       child: Text('STR \n ${vm.abilityScores[0]} (${vm.abilityModifiers[0]}) \n Save: ${vm.getSavingThrows()[0]}',
                           style: TextStyle(fontSize: 18),
                           textAlign: TextAlign.center),
@@ -186,7 +213,10 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                     margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
                     width: 90.0,
                     height: 70.0,
-                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    decoration: BoxDecoration(
+                        border: Border.all(color:const Color(0xFF302727), width: 3),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
                     child: Text('DEX \n ${vm.abilityScores[1]} (${vm.abilityModifiers[1]}) \n Save: ${vm.getSavingThrows()[1]}',
                         style: TextStyle(fontSize: 18),
                         textAlign: TextAlign.center),
@@ -196,7 +226,10 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                     margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
                     width: 90.0,
                     height: 70.0,
-                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    decoration: BoxDecoration(
+                        border: Border.all(color:const Color(0xFF302727), width: 3),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
                     child: Text('CON \n ${vm.abilityScores[2]} (${vm.abilityModifiers[2]}) \n Save: ${vm.getSavingThrows()[2]}',
                         style: TextStyle(fontSize: 18),
                         textAlign: TextAlign.center),
@@ -217,7 +250,10 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                       margin: EdgeInsets.fromLTRB(17.0, 0, 10.0, 0),
                       width: 90.0,
                       height: 70.0,
-                      decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                      decoration: BoxDecoration(
+                          border: Border.all(color:const Color(0xFF302727), width: 3),
+                          borderRadius: BorderRadius.circular(10)
+                      ),
                       child: Text('INT \n ${vm.abilityScores[3]} (${vm.abilityModifiers[3]}) \n Save: ${vm.getSavingThrows()[3]}',
                           style: TextStyle(fontSize: 18),
                           textAlign: TextAlign.center),
@@ -227,7 +263,10 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                     margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
                     width: 90.0,
                     height: 70.0,
-                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    decoration: BoxDecoration(
+                        border: Border.all(color:const Color(0xFF302727), width: 3),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
                     child: Text('WIS \n ${vm.abilityScores[4]} (${vm.abilityModifiers[4]}) \n Save: ${vm.getSavingThrows()[4]}',
                         style: TextStyle(fontSize: 18),
                         textAlign: TextAlign.center),
@@ -238,7 +277,10 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
                     width: 90.0,
                     height: 70.0,
 
-                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    decoration: BoxDecoration(
+                        border: Border.all(color:const Color(0xFF302727), width: 3),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
                     child: Text('CHA \n ${vm.abilityScores[5]} (${vm.abilityModifiers[5]}) \n Save: ${vm.getSavingThrows()[5]}',
                         style: TextStyle(fontSize: 18),
                         textAlign: TextAlign.center),
@@ -252,14 +294,17 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
               width: 400.0,
               height: 20.0,
               color: Color(0xFFAD9090),
-              child: Text('Feats', style: TextStyle(fontSize: 18, color: Colors.black), textAlign: TextAlign.center),
+              child: Text('Features & Traits', style: TextStyle(fontSize: 18), textAlign: TextAlign.center),
             ),
-            Container(
-              margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
-              width: 400.0,
-              height: 1650.0,
-              color: Color(0xFFAD9090),
-              child: Text(vm.featuresToString(), style: TextStyle(fontSize: 18, color: Colors.black), textAlign: TextAlign.center),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+                width: 400.0,
+                height: 1650.0,
+                color: Color(0xFFAD9090),
+                child: Text(vm.featuresToString(), style: TextStyle(fontSize: 18), textAlign: TextAlign.left),
+              ),
             ),
           ],
         )
