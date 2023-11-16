0b0e072ec566319812ebe3fb7140ca9a0c82a5fd
Evan Marnik <79328255+EvanMarnik@users.noreply.github.com>
2023-11-16 14:29:19 -0500

    Update character_sheet_view.dart

--- lib/view/character_sheet_view.dart
+++ lib/view/character_sheet_view.dart
@@ -53,200 +53,217 @@ class _CharacterSheetViewState extends State<CharacterSheetView> {
         title: Text(vm.name,
             style: TextStyle(fontSize: 24, color: Colors.black)),
       ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: [
-              Container(
-                margin: EdgeInsets.all(25.0),
-                width: 80.0,
-                height: 80.0,
-                color: Color(0xFFAD9090),
-                alignment: Alignment.center,
-                child: Text('${vm.armorClass} \n Armor Class',
-                    style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center),
-              ),
-              Container(
-                margin: EdgeInsets.all(25.0),
-                width: 80.0,
-                height: 80.0,
-                color: Color(0xFFAD9090),
-                alignment: Alignment.center,
-                child: Text('+${vm.abilityModifiers[1]} \nInitiative',
-                    style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center),
-              ),
-              Container(
-                margin: EdgeInsets.all(25.0),
-                width: 80.0,
-                height: 80.0,
-                color: Color(0xFFAD9090),
-                alignment: Alignment.center,
-                child: Text('${vm.speed} \nSpeed',
-                    style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                margin: EdgeInsets.all(25.0),
-                width: 80.0,
-                height: 80.0,
-                color: Color(0xFFAD9090),
-                alignment: Alignment.center,
-                child: const Text('+2 \n Proficiency',
-                    style: TextStyle(fontSize: 14),
-                    textAlign: TextAlign.center),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(25, 25, 0, 25),
-                width: 80.0,
-                height: 80.0,
-                color: Color(0xFFAD9090),
-                alignment: Alignment.center,
-                child: Text('${vm.currHitPoints}/${vm.maxHitPoints} \n Hit Points',
-                    style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center),
-              ),
-              Container(
-                margin: EdgeInsets.all(0.0),
-                child: Column(children: <Widget>[
-                  SizedBox(
-                    width: 40,
-                    height: 40,
-                    child: FilledButton(
-                      onPressed: incrementHealth,
-                      style: FilledButton.styleFrom(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                      ),
-                      child: const Text(
-                        '+',
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(25.0),
+                  width: 80.0,
+                  height: 80.0,
+                  color: Color(0xFFAD9090),
+                  alignment: Alignment.center,
+                  child: Text('${vm.armorClass} \n Armor Class',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25.0),
+                  width: 80.0,
+                  height: 80.0,
+                  color: Color(0xFFAD9090),
+                  alignment: Alignment.center,
+                  child: Text('+${vm.abilityModifiers[1]} \nInitiative',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25.0),
+                  width: 80.0,
+                  height: 80.0,
+                  color: Color(0xFFAD9090),
+                  alignment: Alignment.center,
+                  child: Text('${vm.speed} \nSpeed',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(25.0),
+                  width: 80.0,
+                  height: 80.0,
+                  color: Color(0xFFAD9090),
+                  alignment: Alignment.center,
+                  child: const Text('+2 \n Proficiency',
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(25, 25, 0, 25),
+                  width: 80.0,
+                  height: 80.0,
+                  color: Color(0xFFAD9090),
+                  alignment: Alignment.center,
+                  child: Text('${vm.currHitPoints}/${vm.maxHitPoints} \n Hit Points',
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center),
+                ),
+                Container(
+                  margin: EdgeInsets.all(0.0),
+                  child: Column(children: <Widget>[
+                    SizedBox(
                       width: 40,
                       height: 40,
                       child: FilledButton(
-                        onPressed: decrementHealth,
+                        onPressed: incrementHealth,
                         style: FilledButton.styleFrom(
                           padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                         ),
                         child: const Text(
-                          '-',
+                          '+',
                           style: TextStyle(fontSize: 30),
                         ),
-                      ))
-                ]),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(10, 25, 25, 25),
-                width: 80.0,
-                height: 80.0,
-                color: Color(0xFFAD9090),
-                alignment: Alignment.center,
-                child: Text('1d${vm.hitDice} \nHit Dice',
-                    style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center),
-              ),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 0),
-            width: 400.0,
-            height: 20.0,
-            color: Color(0xFFAD9090),
-            child: Text('Ability Scores', style: TextStyle(fontSize: 18, color: Colors.black), textAlign: TextAlign.center),
-          ),
-
-          Container(
-            margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
-            width: 400.0,
-            height: 112.0,
-            color: Color(0xFFAD9090),
-            child: Row(
-              children: [
-                Column(
-                  children: [Container(
-                    margin: EdgeInsets.fromLTRB(17.0, 10.0, 10.0, 0),
-                    width: 90.0,
-                    height: 70.0,
-                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
-                    child: Text('STR \n ${vm.abilityScores[0]} (${vm.abilityModifiers[0]}) \n Save: ${vm.getSavingThrows()[0]}',
-                        style: TextStyle(fontSize: 18),
-                        textAlign: TextAlign.center),
-                  )],
+                      ),
+                    ),
+                    SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: FilledButton(
+                          onPressed: decrementHealth,
+                          style: FilledButton.styleFrom(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          ),
+                          child: const Text(
+                            '-',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ))
+                  ]),
                 ),
-                Column(children: [Container(
-                  margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
-                  width: 90.0,
-                  height: 70.0,
-                  decoration: BoxDecoration(border: Border.all(color:Colors.black)),
-                  child: Text('DEX \n ${vm.abilityScores[1]} (${vm.abilityModifiers[1]}) \n Save: ${vm.getSavingThrows()[1]}',
+                Container(
+                  margin: EdgeInsets.fromLTRB(10, 25, 25, 25),
+                  width: 80.0,
+                  height: 80.0,
+                  color: Color(0xFFAD9090),
+                  alignment: Alignment.center,
+                  child: Text('1d${vm.hitDice} \nHit Dice',
                       style: TextStyle(fontSize: 18),
                       textAlign: TextAlign.center),
-                )],
-                ),
-                Column(children: [Container(
-                  margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
-                  width: 90.0,
-                  height: 70.0,
-                  decoration: BoxDecoration(border: Border.all(color:Colors.black)),
-                  child: Text('CON \n ${vm.abilityScores[2]} (${vm.abilityModifiers[2]}) \n Save: ${vm.getSavingThrows()[2]}',
-                      style: TextStyle(fontSize: 18),
-                      textAlign: TextAlign.center),
-                )],
                 ),
               ],
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
-            width: 400.0,
-            height: 100.0,
-            color: Color(0xFFAD9090),
-            child: Row(
-              children: [
-                Column(
-                  children: [Container(
-                margin: EdgeInsets.fromLTRB(17.0, 0, 10.0, 0),
+            Container(
+              margin: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 0),
+              width: 400.0,
+              height: 20.0,
+              color: Color(0xFFAD9090),
+              child: Text('Ability Scores', style: TextStyle(fontSize: 18, color: Colors.black), textAlign: TextAlign.center),
+            ),
+
+            Container(
+              margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              width: 400.0,
+              height: 112.0,
+              color: Color(0xFFAD9090),
+              child: Row(
+                children: [
+                  Column(
+                    children: [Container(
+                      margin: EdgeInsets.fromLTRB(17.0, 10.0, 10.0, 0),
+                      width: 90.0,
+                      height: 70.0,
+                      decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                      child: Text('STR \n ${vm.abilityScores[0]} (${vm.abilityModifiers[0]}) \n Save: ${vm.getSavingThrows()[0]}',
+                          style: TextStyle(fontSize: 18),
+                          textAlign: TextAlign.center),
+                    )],
+                  ),
+                  Column(children: [Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
                     width: 90.0,
                     height: 70.0,
                     decoration: BoxDecoration(border: Border.all(color:Colors.black)),
-                    child: Text('INT \n ${vm.abilityScores[3]} (${vm.abilityModifiers[3]}) \n Save: ${vm.getSavingThrows()[3]}',
+                    child: Text('DEX \n ${vm.abilityScores[1]} (${vm.abilityModifiers[1]}) \n Save: ${vm.getSavingThrows()[1]}',
                         style: TextStyle(fontSize: 18),
                         textAlign: TextAlign.center),
                   )],
-                ),
-                Column(children: [Container(
-                  margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-                  width: 90.0,
-                  height: 70.0,
-                  decoration: BoxDecoration(border: Border.all(color:Colors.black)),
-                  child: Text('WIS \n ${vm.abilityScores[4]} (${vm.abilityModifiers[4]}) \n Save: ${vm.getSavingThrows()[4]}',
-                      style: TextStyle(fontSize: 18),
-                      textAlign: TextAlign.center),
-                )],
-                ),
-                Column(children: [Container(
-                  margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-                  width: 90.0,
-                  height: 70.0,
-
-                  decoration: BoxDecoration(border: Border.all(color:Colors.black)),
-                  child: Text('CHA \n ${vm.abilityScores[5]} (${vm.abilityModifiers[5]}) \n Save: ${vm.getSavingThrows()[5]}',
-                      style: TextStyle(fontSize: 18),
-                      textAlign: TextAlign.center),
-                )],
-                ),
-              ],
+                  ),
+                  Column(children: [Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
+                    width: 90.0,
+                    height: 70.0,
+                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    child: Text('CON \n ${vm.abilityScores[2]} (${vm.abilityModifiers[2]}) \n Save: ${vm.getSavingThrows()[2]}',
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center),
+                  )],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              width: 400.0,
+              height: 100.0,
+              color: Color(0xFFAD9090),
+              child: Row(
+                children: [
+                  Column(
+                    children: [Container(
+                      margin: EdgeInsets.fromLTRB(17.0, 0, 10.0, 0),
+                      width: 90.0,
+                      height: 70.0,
+                      decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                      child: Text('INT \n ${vm.abilityScores[3]} (${vm.abilityModifiers[3]}) \n Save: ${vm.getSavingThrows()[3]}',
+                          style: TextStyle(fontSize: 18),
+                          textAlign: TextAlign.center),
+                    )],
+                  ),
+                  Column(children: [Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                    width: 90.0,
+                    height: 70.0,
+                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    child: Text('WIS \n ${vm.abilityScores[4]} (${vm.abilityModifiers[4]}) \n Save: ${vm.getSavingThrows()[4]}',
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center),
+                  )],
+                  ),
+                  Column(children: [Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                    width: 90.0,
+                    height: 70.0,
+
+                    decoration: BoxDecoration(border: Border.all(color:Colors.black)),
+                    child: Text('CHA \n ${vm.abilityScores[5]} (${vm.abilityModifiers[5]}) \n Save: ${vm.getSavingThrows()[5]}',
+                        style: TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center),
+                  )],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 0),
+              width: 400.0,
+              height: 20.0,
+              color: Color(0xFFAD9090),
+              child: Text('Feats', style: TextStyle(fontSize: 18, color: Colors.black), textAlign: TextAlign.center),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              width: 400.0,
+              height: 300.0,
+              color: Color(0xFFAD9090),
+              //child: Text(vm.featuresToString(), style: TextStyle(fontSize: 18, color: Colors.black), textAlign: TextAlign.center),
+            ),
+          ],
+        )
+
       ),
     );
   }
