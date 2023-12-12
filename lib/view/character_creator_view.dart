d02878db16d477426529ffc514f79650e76b36ad
Evan Marnik <79328255+EvanMarnik@users.noreply.github.com>
2023-12-12 15:16:31 -0500

    Update character_creator_view.dart

--- lib/view/character_creator_view.dart
+++ lib/view/character_creator_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:dnd_character_sheet_app/constants.dart';
 import 'package:dnd_character_sheet_app/view_model/character_list_view_model.dart';
 import 'package:dnd_character_sheet_app/view/character_list_view.dart';
+import 'package:flutter/services.dart';
 import '../model/character_model.dart';
 import '../model/character_option.dart';
 
@@ -27,7 +28,8 @@ class _CharacterCreatorViewState extends State<CharacterCreatorView> {
   @override
   void initState() {
     super.initState();
-    vm = CharacterCreatorViewModel(characterListViewModel: characterListViewModel);
+    vm = CharacterCreatorViewModel(
+        characterListViewModel: characterListViewModel);
   }
 
   CharacterOption? selectedClass = barbarian;
@@ -37,117 +39,207 @@ class _CharacterCreatorViewState extends State<CharacterCreatorView> {
   List<CharacterOption> classList = classes;
   List<CharacterOption> raceList = races;
   List<CharacterOption> backgroundList = backgrounds;
+  int strength = 10;
+  int dexterity = 10;
+  int constitution = 10;
+  int intelligence = 10;
+  int wisdom = 10;
+  int charisma = 10;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Color(0xFFAD9090),
+        backgroundColor: Color(0xFFAD9090),
         appBar: AppBar(
           backgroundColor: Color(0xFF302727),
           title: const Text('Character Creator',
               style: TextStyle(color: const Color(0xFFAD9090))),
         ),
         body: Center(
-
             child: Column(
+
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-                hintText: 'Enter Name', iconColor: Color(0xFFAD9090)),
-              onChanged: (text) {
-                selectedName = text;
-              }
-          ),
-          const Text(
-            'Select Class',
-            style: TextStyle(
-              color: Color(0xFF302727),
-            ),
-          ),
-          DropdownMenu(
-              dropdownMenuEntries: classList
-                  .map<DropdownMenuEntry<CharacterOption>>(
-                      (CharacterOption value) {
-                return DropdownMenuEntry<CharacterOption>(
-                    value: value, label: value.name);
-              }).toList(),
-              initialSelection: classList.first,
-              menuStyle: MenuStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
-              onSelected: (CharacterOption? classPick) {
-                setState(() {
-                  selectedClass = classPick;
-                });
-              }),
-          const Text(
-            'Select Race',
-            style: TextStyle(
-              color: Color(0xFF302727),
-            ),
-          ),
-          DropdownMenu(
-              dropdownMenuEntries: raceList
-                  .map<DropdownMenuEntry<CharacterOption>>(
-                      (CharacterOption value) {
-                return DropdownMenuEntry<CharacterOption>(
-                    value: value, label: value.name);
-              }).toList(),
-              initialSelection: raceList.first,
-              menuStyle: MenuStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
-              onSelected: (CharacterOption? racePick) {
-                setState(() {
-                  selectedRace = racePick;
-                });
-              }),
-          const Text(
-            'Select Background',
-            style: TextStyle(
-              color: Color(0xFF302727),
-            ),
-          ),
-          DropdownMenu(
-              dropdownMenuEntries: backgroundList
-                  .map<DropdownMenuEntry<CharacterOption>>(
-                      (CharacterOption value) {
-                return DropdownMenuEntry<CharacterOption>(
-                    value: value, label: value.name);
-              }).toList(),
-              initialSelection: backgroundList.first,
-              menuStyle: MenuStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
-              onSelected: (CharacterOption? bgPick) {
-                setState(() {
-                  selectedBG = bgPick;
-                });
-              }),
-          SizedBox(
+              TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Enter Name', iconColor: Color(0xFFAD9090), contentPadding: EdgeInsets.symmetric(horizontal: 40)),
 
-              width: 350,
-              child: FilledButton(
-
-                style: FilledButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 25),
+                  onChanged: (text) {
+                    selectedName = text;
+                  }),
+              const Text(
+                'Select Class',
+                style: TextStyle(
+                  color: Color(0xFF302727),
                 ),
-                onPressed: () {
-                  vm.setCharClass(selectedClass!);
-                  vm.setRace(selectedRace!);
-                  vm.setBackground(selectedBG!);
-                  vm.setName(selectedName);
-                  vm.constructCharacter();
-                  Navigator.pushNamed(context, listRoute,
-                      arguments: characterListViewModel);
-                  Navigator.pushNamed(context, sheetRoute,
-                      arguments: vm.character);
-                },
-                child: const Text('Create Character'),
-              ))
-        ])));
+              ),
+              DropdownMenu(
+                  dropdownMenuEntries: classList
+                      .map<DropdownMenuEntry<CharacterOption>>(
+                          (CharacterOption value) {
+                    return DropdownMenuEntry<CharacterOption>(
+                        value: value, label: value.name);
+                  }).toList(),
+                  initialSelection: classList.first,
+                  menuStyle: MenuStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
+                  onSelected: (CharacterOption? classPick) {
+                    setState(() {
+                      selectedClass = classPick;
+                    });
+                  }),
+              const Text(
+                'Select Race',
+                style: TextStyle(
+                  color: Color(0xFF302727),
+                ),
+              ),
+              DropdownMenu(
+                  dropdownMenuEntries: raceList
+                      .map<DropdownMenuEntry<CharacterOption>>(
+                          (CharacterOption value) {
+                    return DropdownMenuEntry<CharacterOption>(
+                        value: value, label: value.name);
+                  }).toList(),
+                  initialSelection: raceList.first,
+                  menuStyle: MenuStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
+                  onSelected: (CharacterOption? racePick) {
+                    setState(() {
+                      selectedRace = racePick;
+                    });
+                  }),
+              const Text(
+                'Select Background',
+                style: TextStyle(
+                  color: Color(0xFF302727),
+                ),
+              ),
+              DropdownMenu(
+                  dropdownMenuEntries: backgroundList
+                      .map<DropdownMenuEntry<CharacterOption>>(
+                          (CharacterOption value) {
+                    return DropdownMenuEntry<CharacterOption>(
+                        value: value, label: value.name);
+                  }).toList(),
+                  initialSelection: backgroundList.first,
+                  menuStyle: MenuStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
+                  onSelected: (CharacterOption? bgPick) {
+                    setState(() {
+                      selectedBG = bgPick;
+                    });
+                  }),
+                  SizedBox(
+                    width: 100,
+                    child: TextField(
+                        decoration: InputDecoration(
+                            hintText: 'STR', iconColor: Color(0xFFAD9090)),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (text) {
+                          strength = int.parse(text);
+                        })
+
+                  ),
+                  SizedBox(
+                      width: 100,
+                      child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'DEX', iconColor: Color(0xFFAD9090)),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          onChanged: (text) {
+                            dexterity = int.parse(text);
+                          })
+
+                  ),
+                  SizedBox(
+                      width: 100,
+                      child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'CON', iconColor: Color(0xFFAD9090)),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          onChanged: (text) {
+                            constitution = int.parse(text);
+                          })
+
+                  ),
+                  SizedBox(
+                      width: 100,
+                      child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'INT', iconColor: Color(0xFFAD9090)),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          onChanged: (text) {
+                            intelligence = int.parse(text);
+                          })
+
+                  ),
+                  SizedBox(
+                      width: 100,
+                      child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'WIS', iconColor: Color(0xFFAD9090)),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          onChanged: (text) {
+                            wisdom = int.parse(text);
+                          })
+
+                  ),
+                  SizedBox(
+                      width: 100,
+                      child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'CHA', iconColor: Color(0xFFAD9090)),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          onChanged: (text) {
+                            charisma = int.parse(text);
+                          })
+
+                  ),
+
+              SizedBox(
+                  width: 350,
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 25),
+                    ),
+                    onPressed: () {
+                      vm.setCharClass(selectedClass!);
+                      vm.setRace(selectedRace!);
+                      vm.setBackground(selectedBG!);
+                      vm.setName(selectedName);
+                      vm.setAbilityScores([strength, dexterity, constitution, intelligence, wisdom, charisma]);
+                      vm.constructCharacter();
+                      Navigator.pushNamed(context, listRoute,
+                          arguments: characterListViewModel);
+                      Navigator.pushNamed(context, sheetRoute,
+                          arguments: vm.character);
+                    },
+                    child: const Text('Create Character'),
+                  ))
+            ])));
   }
 }
