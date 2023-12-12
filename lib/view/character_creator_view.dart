5428c2250b532f4c1899712e6b7604d9c3e12385
Evan Marnik <79328255+EvanMarnik@users.noreply.github.com>
2023-12-12 01:08:36 -0500

    Update character_creator_view.dart

--- lib/view/character_creator_view.dart
+++ lib/view/character_creator_view.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
+
+import 'package:dnd_character_sheet_app/view_model/character_creator_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:dnd_character_sheet_app/constants.dart';
 import 'package:dnd_character_sheet_app/view_model/character_list_view_model.dart';
-
+import 'package:dnd_character_sheet_app/view/character_list_view.dart';
 import '../model/character_model.dart';
 import '../model/character_option.dart';
 
@@ -19,116 +22,134 @@ class _CharacterCreatorViewState extends State<CharacterCreatorView> {
   CharacterListViewModel characterListViewModel;
 
   _CharacterCreatorViewState({required this.characterListViewModel});
- CharacterOption? selectedClass = barbarian;
-  String selectedRace = 'dragonborn';
-  String selectedBG = 'acolyte';
-  List<CharacterOption> classList = classes;
-  List<String> raceList = ['Dragonborn','Dwarf','Elf','Gnome','Half Elf','Halfling','Half Orc','Human','Tiefling'];
-  List<String> backgroundList = ['Acolyte','Charlatan', 'Criminal', 'Entertainer', 'Folk Hero', 'Guild Artisan', 'Hermit', 'Noble',
-    'Outlander', 'Sage', 'Sailor', 'Soldier', 'Urchin'];
 
+  CharacterOption? selectedClass = barbarian;
+  CharacterOption? selectedRace = dragonborn;
+  CharacterOption? selectedBG = acolyte;
+  String selectedName = "";
+  List<CharacterOption> classList = classes;
+  List<CharacterOption> raceList = races;
+  List<CharacterOption> backgroundList = backgrounds;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      backgroundColor: Color(0xFFAD9090),
         appBar: AppBar(
-          backgroundColor: Color(0xFFAD9090),
+          backgroundColor: Color(0xFF302727),
           title: const Text('Character Creator',
-              style: TextStyle(color: const Color(0xFF302727))),
+              style: TextStyle(color: const Color(0xFFAD9090))),
         ),
         body: Center(
 
             child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  const Text(
-                    'Select Class',
-                    style: TextStyle(
-                      color: Color(0xFFAD9090),
-                    ),
-                  ),
-              DropdownMenu(
+          TextField(
+            decoration: InputDecoration(
+                hintText: 'Enter Name', iconColor: Color(0xFFAD9090)),
+              onChanged: (text) {
+                selectedName = text;
+              }
+          ),
+          const Text(
+            'Select Class',
+            style: TextStyle(
+              color: Color(0xFF302727),
+            ),
+          ),
+          DropdownMenu(
+              dropdownMenuEntries: classList
+                  .map<DropdownMenuEntry<CharacterOption>>(
+                      (CharacterOption value) {
+                return DropdownMenuEntry<CharacterOption>(
+                    value: value, label: value.name);
+              }).toList(),
+              initialSelection: classList.first,
+              menuStyle: MenuStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
+              onSelected: (CharacterOption? classPick) {
+                setState(() {
+                  selectedClass = classPick;
+                });
+              }),
+          const Text(
+            'Select Race',
+            style: TextStyle(
+              color: Color(0xFF302727),
+            ),
+          ),
+          DropdownMenu(
+              dropdownMenuEntries: raceList
+                  .map<DropdownMenuEntry<CharacterOption>>(
+                      (CharacterOption value) {
+                return DropdownMenuEntry<CharacterOption>(
+                    value: value, label: value.name);
+              }).toList(),
+              initialSelection: raceList.first,
+              menuStyle: MenuStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
+              onSelected: (CharacterOption? racePick) {
+                setState(() {
+                  selectedRace = racePick;
+                });
+              }),
+          const Text(
+            'Select Background',
+            style: TextStyle(
+              color: Color(0xFF302727),
+            ),
+          ),
+          DropdownMenu(
+              dropdownMenuEntries: backgroundList
+                  .map<DropdownMenuEntry<CharacterOption>>(
+                      (CharacterOption value) {
+                return DropdownMenuEntry<CharacterOption>(
+                    value: value, label: value.name);
+              }).toList(),
+              initialSelection: backgroundList.first,
+              menuStyle: MenuStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Color(0xFFAD9090))),
+              onSelected: (CharacterOption? bgPick) {
+                setState(() {
+                  selectedBG = bgPick;
+                });
+              }),
+          SizedBox(
 
-                dropdownMenuEntries:
-                    classList.map<DropdownMenuEntry<CharacterOption>>((CharacterOption value) {
-                  return DropdownMenuEntry<CharacterOption>(value: value, label: value.name);
-                }).toList(),
-                initialSelection: classList.first,
+              width: 350,
+              child: FilledButton(
 
-                menuStyle: MenuStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFFAD9090))
+                style: FilledButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 25),
                 ),
-                  onSelected: (CharacterOption? classPick) {
-                    setState(() {
-                      selectedClass= classPick;
-                    });
-                  }
-              ),
-                  const Text(
-                    'Select Race',
-                    style: TextStyle(
-                      color: Color(0xFFAD9090),
-                    ),
-                  ),
-                  DropdownMenu(
-
-                    dropdownMenuEntries:
-                    raceList.map<DropdownMenuEntry<String>>((String value) {
-                      return DropdownMenuEntry<String>(value: value, label: value);
-                    }).toList(),
-                    initialSelection: raceList.first,
-
-                    menuStyle: MenuStyle(
-                        backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFFAD9090))
-                    ),
-                  ),
-                  const Text(
-                    'Select Background',
-                    style: TextStyle(
-                      color: Color(0xFFAD9090),
-                    ),
-                  ),
-                  DropdownMenu(
-
-                    dropdownMenuEntries:
-                    backgroundList.map<DropdownMenuEntry<String>>((String value) {
-                      return DropdownMenuEntry<String>(value: value, label: value);
-                    }).toList(),
-                    initialSelection: backgroundList.first,
-
-                    menuStyle: MenuStyle(
-                        backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFFAD9090))
-                    ),
-                  ),
-
-                  SizedBox(
-                      width: 350,
-                      child: FilledButton(
-                        style: FilledButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 25),
-                        ),
-                        onPressed: (){
-                          Navigator.pushNamed(context, sheetRoute, arguments: CharacterModel(
-                            name: "Coddrin Thunderslayer",
-                            charClass: selectedClass!,
-                            subclass: null,
-                            background: folkHero,
-                            race: halfOrc,
-                            abilityScores: [16, 14, 16, 8, 10, 8],
-                            spells: null,
-                            maxSpellSlots: null,
-                            currSpellSlots: null,
-                            maxHitPoints: 15,
-                            currHitPoints: 15,
-                            armorClass: 15,
-                            speed: 30,
-                            hitDice: 12,
-                          ));
-                        },
-                        child: const Text('Create Character'),
-                      )
-                  )
-
-            ])));
+                onPressed: () {
+                  Navigator.pushNamed(context, sheetRoute,
+                      arguments: CharacterModel(
+                        name: selectedName,
+                        charClass: selectedClass!,
+                        subclass: null,
+                        background: selectedBG!,
+                        race: selectedRace!,
+                        abilityScores: [16, 14, 16, 8, 10, 8],
+                        spells: null,
+                        maxSpellSlots: null,
+                        currSpellSlots: null,
+                        maxHitPoints: 15,
+                        currHitPoints: 15,
+                        armorClass: 15,
+                        speed: 30,
+                        hitDice: 12,
+                      ));
+                  //CharacterCreatorViewModel(characterListViewModel: characterListViewModel).constructCharacter();
+                },
+                child: const Text('Create Character'),
+              ))
+        ])));
   }
 }
